634160cae0facb1d6d8f704ded57a1023c77f477
zoink506 <benhubble03@gmail.com>
2023-06-02 13:30:50 +1000

    Implement commenting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_122606) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_030309) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "link"
@@ -26,5 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_122606) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
